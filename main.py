c8b6a5c93d501d95993fb8d515c3b24ff8fb3c22
Serban Costandache <serbanc94@gmail.com>
2017-10-15 18:16:45 +0300

    wip

--- main.py
+++ main.py
@@ -6,61 +6,35 @@ f.close()
 def activation(input):
     max_val = np.max(input)
     print input
-    return np.where(max_val==input)[0]
+    return np.where(max_val==input)[0][0]
 
 
 def normalize(input):
     s = sum(input)
-    max_val = max(input)
-    min_val = min(input)
-    norm = np.array([(i-min_val) / (max_val-min_val) for i in input])
+    norm = np.array([float(i) / s for i in input])
     return norm
 
 
-"""def train(train_set, nr_iter, train_rate):
-    #PERCEPTRONS = np.zeros((10, 784), dtype=np.float)
-    PERCEPTRONS = np.random.uniform(low=0.0, high=0.3, size=(10,784))
+def train(train_set, nr_iter, train_rate):
+
+    PERCEPTRONS = np.random.uniform(low=0.0, high=0.3, size=(784,10))
     all_classified = False
 
     while(not(all_classified) and nr_iter > 0 ):
         print nr_iter
         all_classified = True
-        if(nr_iter < 20):
-            train_rate = 0.5
-        if(nr_iter < 10):
-            train_rate = 0.2
 
         for (digit_arr, target) in zip(train_set[0], train_set[1]):
-            dot_prod = np.dot(PERCEPTRONS, digit_arr)
-            output = activation(dot_prod)
-            print target
-            first_tup = (target-output)*digit_arr*train_rate
-            PERCEPTRONS[target] = PERCEPTRONS[target] + first_tup
-            #change target function!
-            if(output != target):
-                all_classified = False
-        nr_iter-=1
-
-    return PERCEPTRONS
-"""
-
-def train(train_set, nr_iter, train_rate):
-    PERCEPTRONS = np.random.uniform(low=0.0, high = 0.3, size=(784,10))
-    all_classified = False
-
-    while(all_classified == False):
-        all_classified = True
-        good_class = 0
-        for(digit_arr, target) in zip(train_set[0], train_set[1]):
-            pxl_mat = np.mat(digit_arr)
             dot_prod = np.dot(digit_arr, PERCEPTRONS)
             output = activation(dot_prod)
-            PERCEPTRONS[:, target] = PERCEPTRONS[:, target]+(target-output)*pxl_mat*train_rate
-            if output!=target:
-                all_classified=False
-            else:
-                good_class+=1
-        print good_class
+            print output
+
+            PERCEPTRONS[:,target] = PERCEPTRONS[:,target]+(target-output)*digit_arr*train_rate
+
+
+
+    return PERCEPTRONS
+
 
 
 def test(test_set, weights):
