2aee4b30f3fb28ad9f6e75f9a581811d81e336e4
Serban Costandache <serbanc94@gmail.com>
2017-10-15 17:48:20 +0300

    wip

--- main.py
+++ main.py
@@ -3,35 +3,24 @@ f = gzip.open('mnist.pkl.gz', 'rb')
 train_set, valid_set, test_set = cPickle.load(f)
 f.close()
 
-
-def activation_unbiased(dot_result, target):
-    max_val = np.max(dot_result)
-    if (max_val != 0.0):
-        if(np.where(dot_result == max_val) == target):
-            return target
-    return 0
-
-
 def activation(input):
     max_val = np.max(input)
-    if( max_val >= 0.5):
-       return np.where(input == max_val)[0]
-    return 0
+    print input
+    return np.where(max_val==input)[0]
+
 
 def normalize(input):
     s = sum(input)
-    norm = np.array([float(i) / s for i in input])
+    max_val = max(input)
+    min_val = min(input)
+    norm = np.array([(i-min_val) / (max_val-min_val) for i in input])
     return norm
 
-def arr_value(value):
-    aux_arr = np.zeros(10, dtype=float)
-    aux_arr[value] = 1.0
-    return aux_arr
 
-def train(train_set, nr_iter, train_rate):
-    PERCEPTRONS = np.zeros((10, 784), dtype=np.float)
+"""def train(train_set, nr_iter, train_rate):
+    #PERCEPTRONS = np.zeros((10, 784), dtype=np.float)
+    PERCEPTRONS = np.random.uniform(low=0.0, high=0.3, size=(10,784))
     all_classified = False
-    bias = np.zeros(10, dtype=np.float)
 
     while(not(all_classified) and nr_iter > 0 ):
         print nr_iter
@@ -42,22 +31,42 @@ def train(train_set, nr_iter, train_rate):
             train_rate = 0.2
 
         for (digit_arr, target) in zip(train_set[0], train_set[1]):
-            z = normalize(np.dot(PERCEPTRONS, digit_arr))+bias
-            output = activation(z)
-            PERCEPTRONS[target] = PERCEPTRONS[target] + (target-output)*digit_arr*train_rate
-            bias = bias + (arr_value(target)-arr_value(output))*train_rate
+            dot_prod = np.dot(PERCEPTRONS, digit_arr)
+            output = activation(dot_prod)
+            print target
+            first_tup = (target-output)*digit_arr*train_rate
+            PERCEPTRONS[target] = PERCEPTRONS[target] + first_tup
             #change target function!
             if(output != target):
                 all_classified = False
         nr_iter-=1
 
-    return PERCEPTRONS, bias
+    return PERCEPTRONS
+"""
+
+def train(train_set, nr_iter, train_rate):
+    PERCEPTRONS = np.random.uniform(low=0.0, high = 0.3, size=(784,10))
+    all_classified = False
+
+    while(all_classified == False):
+        all_classified = True
+        good_class = 0
+        for(digit_arr, target) in zip(train_set[0], train_set[1]):
+            pxl_mat = np.mat(digit_arr)
+            dot_prod = np.dot(digit_arr, PERCEPTRONS)
+            output = activation(dot_prod)
+            PERCEPTRONS[:, target] = PERCEPTRONS[:, target]+(target-output)*pxl_mat*train_rate
+            if output!=target:
+                all_classified=False
+            else:
+                good_class+=1
+        print good_class
 
 
-def test(test_set, weights, bias):
+def test(test_set, weights):
     well_classified = 0
     for (digit_arr, target) in zip(test_set[0], test_set[1]):
-        z = normalize(np.dot(weights, digit_arr) + bias)
+        z = normalize(np.dot(weights, digit_arr) )
 
         if(target == activation(z)):
             well_classified +=1
@@ -65,7 +74,9 @@ def test(test_set, weights, bias):
     return (100.0 * well_classified)/len(test_set[0])
 
 
-WEIGHTS, bias = train(train_set, 1, 0.7)
+WEIGHTS = train(train_set, 2, 0.7)
 
-print test(test_set, WEIGHTS, bias)
+print test(test_set, WEIGHTS)
+
+
 
