64535bec163acd3f166fdab19d1e62ed72e24134
Serban Costandache <serbanc94@gmail.com>
2017-10-27 21:33:56 +0300

    added training on all perceptrons at a time, new normalization function

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 import cPickle
 import gzip
 import numpy as np
-import scipy.stats as ss
-from sklearn.preprocessing import scale
-from sklearn.preprocessing import normalize
 
 f = gzip.open('mnist.pkl.gz', 'rb')
 train_set, valid_set, test_set = cPickle.load(f)
 f.close()
-
-
-def normalize_custom(dot_result):
-    # min_val = np.min(dot_result)
-    # max_val = np.max(dot_result)
-    # return np.array([(x - min_val)/(max_val-min_val) for x in dot_result])
-    return scale(dot_result, axis=0, with_mean=True, with_std=True, copy=True)
 
 
 def max_activation(dot_result):
@@ -22,33 +12,43 @@ def max_activation(dot_result):
     return np.where(dot_result == max_val)[0]
 
 
+def mask_normalize(dot_result):
+    min_val = abs(np.min(dot_result)) + 1.0
+    masked_sum = len(dot_result) * min_val + np.sum(dot_result)
+    return np.array([(float(x)+min_val)/masked_sum for x in dot_result])
+
+
+def target_arr(target):
+    target_result = np.array(10 * [0.0])
+    target_result[target] = 1.0
+    return target_result
+
+
 def train(trainer_set, nr_iter, learn_rate):
     #   weights_mat = np.zeros((10, 784), dtype=np.float)
-    weights_mat = np.random.uniform(low=-2.0 / 28, high=2.0 / 28, size=(10, 784))
+    weights_mat = np.random.uniform(low=-1.0/28, high=1.0/28, size=(10, 784))
     bias_arr = np.array(10 * [0.0])
 
-    success_rate = 0.0
-    while success_rate < 88.0:
+    while nr_iter > 0:
         all_number = len(trainer_set[0])
 
         for (pxl_arr, target) in zip(trainer_set[0], trainer_set[1]):
 
-            z = normalize((np.dot(weights_mat, pxl_arr) + bias_arr).reshape(1, -1))[0]
+            z = mask_normalize(np.dot(weights_mat, pxl_arr) + bias_arr)
             output_class = max_activation(z)
-            true_output = z[target]
+            true_output = target_arr(target)
 
-            error = 1.0 - true_output
-            # if output_class != target:
-            weights_mat[target] += pxl_arr * error * learn_rate
-            bias_arr[target] += error * learn_rate
-            weights_mat[output_class] -= pxl_arr * error * learn_rate
-            bias_arr[output_class] -= error * learn_rate
+            if output_class != target:
+                error_arr = true_output - z
+                for idx, error in enumerate(error_arr):
+                    weights_mat[idx] += pxl_arr * error * learn_rate
+                    bias_arr[idx] += error * learn_rate
 
             if output_class != target:
                 all_number -= 1
         success_rate = (100.0 * all_number) / len(trainer_set[0])
         print success_rate
-        nr_iter += 1
+        nr_iter -= 1
         print nr_iter
     return weights_mat, bias_arr
 
@@ -57,7 +57,7 @@ def test(tester_set, weights, bias_arr):
     well_classified = 0
     for (digit_arr, target) in zip(tester_set[0], tester_set[1]):
 
-        z = normalize((np.dot(weights, digit_arr) + bias_arr).reshape(1, -1))[0]
+        z = mask_normalize(np.dot(weights, digit_arr) + bias_arr)
         output_class = max_activation(z)
         if output_class == target:
             well_classified += 1
@@ -75,7 +75,7 @@ def multiple_perceptron_test(tester_set, weights, bias_arr):
     return (100.0 * correct) / len(tester_set[0])
 
 
-WEIGHTS, bias = train(train_set, 0, 0.05)
+WEIGHTS, bias = train(train_set, 20, 0.05)
 print WEIGHTS, bias
 
 print test(test_set, WEIGHTS, bias)
@@ -89,6 +89,3 @@ with open('weights_file', 'w') as outfile:
 with open('bias_file_out', 'w') as outfile:
     for b in bias:
         outfile.write(str(b) + " ")
-
-
-# print normalize(np.array([1,0,0,0,0,-1,0,0,0,0], dtype = np.float).reshape(1,-1))
